737b511ac14a45dfb6b58640763308b50ba662f9
Ivan.D <aryodeliae@gmail.com>
2026-01-20 18:19:42 +0100

    Carte large + georeferencé

--- main.py
+++ main.py
@@ -1,52 +1,44 @@
 import streamlit as st 
 import os 
-#import streamlit.components.v1 as components
 import folium
 from streamlit_folium import st_folium
+import rasterio as rio
 
-#path = os.path.join(os.getcwd(), "Pages", "Page_2.py")
-backg_path = os.path.join(os.getcwd(), "Data", "Expo_univ_1889_Elevage_enfants_premier_age_Finistere_41.jpg")
+with open('style2.css') as f:
+    css = f.read()
 
-st.set_page_config(
-    layout="wide",
-)
-
-#st.sidebar.success('select page')
-
-#st.page_link(path, icon="🔥")
-
-image = "http://www.collections.musee-bretagne.fr/flora_rennes/ark:/83011/0031519888/doc/2272005"
-
-widh_media = 2500
-height_media = 1878
-
-#st.image(image)
+st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
 
 
-m = folium.Map(crs='Simple', zoom_start=0, tiles=None) #
+backg_path = os.path.join(os.getcwd(), "Data", "Carte_SRC_def_img.tif")
+
+
+
+with rio.open(backg_path) as src:
+    
+    img = src.read()
+
+    min_lon, min_lat, max_lon, max_lat = src.bounds
+    bounds_orig = [[min_lat, min_lon], [max_lat, max_lon]]
+
+    centre_lon = bounds_orig[0][1] + (bounds_orig[1][1] - bounds_orig[0][1])/2
+    centre_lat = bounds_orig[0][0] + (bounds_orig[1][0] - bounds_orig[0][0])/2
+
+
+m = folium.Map(location=[centre_lat, centre_lon], zoom_start=9.5)
 
 aldrune_overlay = folium.raster_layers.ImageOverlay(
-    image='./Data/Expo_univ_1889_Elevage_enfants_premier_age_Finistere_7.jpg',
-    bounds=[[0, 0], [1800, 2000]],
+    img.transpose(1, 2, 0),
+    bounds=bounds_orig,
+    opacity=1,
     zindex=1)
+
 aldrune_overlay.add_to(m)
-m.fit_bounds(bounds=[[0, 0], [1800, 2000]])
+#folium.LayerControl().add_to(m)
+#m.fit_bounds(bounds=bounds_orig)
 
 st_data = st_folium(m, use_container_width=True)
 
-places = [
-    {"name": "Paris — University", "lat": 48.8566, "lon": 2.3522, "desc": "University years"},
-    {"name": "Tehran — Birthplace", "lat": 35.6892, "lon": 51.3890, "desc": "Where it all began"},
-    {"name": "Berlin — Future Work", "lat": 52.52, "lon": 13.405, "desc": "Dream job city"}
-]
 
 
-st.markdown(
-        r"""
-        <style>
-        .stAppViewBlockContainer {
-               padding: 0rem;
-            }
-        </style>
-        """, unsafe_allow_html=True
-    )
+
