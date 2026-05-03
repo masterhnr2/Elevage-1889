84125cc57b2a7f15fbe6836af61d913f5b69738f
unknown <come.milene@gmail.com>
2026-05-03 14:02:22 +0200

    changement menu

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,18 @@
 
 <script>
 
-    import Navbar from '$lib/components/Navbar.svelte';
     import IconeLivre from '$lib/components/Icone_livre.svelte';
 
 </script>
 
-<Navbar/>
+<nav>
+<ul>
+    <li><a href="/">Accueil</a></li>
+    <li><a href="/folium">Carte interactive</a></li>
+    <li><a href="/apropos">À propos</a></li>
+</ul>
+</nav>
+
 
 <div class="bod">
 
@@ -43,7 +49,42 @@
 <IconeLivre/>
 
 <style>
+    nav{
+        width : 100%;
+        margin : 0 auto;
+        background-color : white;
+        position : sticky;
+        top : 0px;
+    }
+    nav a{
+        display : block;
+        text-decoration : none;
+        color : #be9f82;
+        border-bottom : 2px solid transparent;
+        padding : 10px 0px;
+        font-size : 23px;
+    }
+    nav a:hover{
+        color :#3b2224;
+        border-bottom : 2px solid #be9f82;
+    }
+    nav ul{
+        list-style-type : none;
+        display : flex;
+        justify-content : center;
+        font-family : Verdana, Geneva, Tahoma, sans-serif;
+        font-weight : bold;
+    }
 
+    nav li{
+        width : 25%;
+        text-align : center;
+    }
+    nav ul::after{
+        content : "";
+        display : table;
+        clear : both;
+    }
 
     .bod {
         padding-top: 5%;
--- src/routes/apropos/+page.svelte
+++ src/routes/apropos/+page.svelte
@@ -1,11 +1,16 @@
 <script>
 
-    import Navbar from '$lib/components/Navbar.svelte';
     import IconeLivre from '$lib/components/Icone_livre.svelte';
 
 </script>
 
-<Navbar/>
+<nav>
+<ul>
+    <li><a href="/">Accueil</a></li>
+    <li><a href="/folium">Carte interactive</a></li>
+    <li><a href="/apropos">À propos</a></li>
+</ul>
+</nav>
 
 <div class="bod">
 <h1>Le Master Humanités Numériques</h1>
@@ -60,6 +65,43 @@
 
 
 <style>
+
+    nav{
+        width : 100%;
+        margin : 0 auto;
+        background-color : white;
+        position : sticky;
+        top : 0px;
+    }
+    nav a{
+        display : block;
+        text-decoration : none;
+        color : #be9f82;
+        border-bottom : 2px solid transparent;
+        padding : 10px 0px;
+        font-size : 23px;
+    }
+    nav a:hover{
+        color :#3b2224;
+        border-bottom : 2px solid #be9f82;
+    }
+    nav ul{
+        list-style-type : none;
+        display : flex;
+        justify-content : center;
+        font-family : Verdana, Geneva, Tahoma, sans-serif;
+        font-weight : bold;
+    }
+
+    nav li{
+        width : 25%;
+        text-align : center;
+    }
+    nav ul::after{
+        content : "";
+        display : table;
+        clear : both;
+    }
 
 
     .bod {
